eb7cc57b98f6f1cf56fd52b9f582379e39e41725
yarpshe <yar.pshe@gmail.com>
2024-07-08 16:09:26 +0300

    er1

--- modules/scrapper_thestore.py
+++ modules/scrapper_thestore.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config_chrome import CHROME_OPTIONS, CHROME_SERVICE
+from modules.config_chrome import CHROME_OPTIONS, CHROME_SERVICE
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import re
