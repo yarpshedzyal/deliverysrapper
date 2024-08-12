36eaf8181830d69b0b49f25b38b79f6b1530ee5e
yarpshe <yar.pshe@gmail.com>
2024-08-12 22:52:16 +0300

    try/finnaly for browser windows to close

--- modules/scrapper_thestore.py
+++ modules/scrapper_thestore.py
@@ -33,84 +33,87 @@ def scrap_status_thestore(the_store_set: set):
     number_of_orders = len(the_store_set)
     counter_for_done = 0
 
-    for order_id in the_store_set:
-        try:
-            driver.get('https://www.therestaurantstore.com/login')
-            print('came to titel')
-            # Wait for the email field to be present
-            email_field = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, 'order-email'))
-            )
-            print('order-email loaded')
-            order_number_field = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, 'order-number'))
-            )
-            print('order-number loaded')
+    try:
+        for order_id in the_store_set:
+            try:
+                driver.get('https://www.therestaurantstore.com/login')
+                print('came to titel')
+                # Wait for the email field to be present
+                email_field = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.ID, 'order-email'))
+                )
+                print('order-email loaded')
+                order_number_field = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.ID, 'order-number'))
+                )
+                print('order-number loaded')
 
-            email_field.send_keys('alina.troian.proton@gmail.com')
-            order_number_field.send_keys(str(order_id))
-            print('keys sended')
-            # Wait for the submit button to be clickable
-            submit_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '#track-orders-form > div > div > div > button'))
-            )
-            submit_button.click()
-            print('clicked button')
-            # Verify if the current page is the order status page
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#main > div:nth-child(4) > div > div:nth-child(1) > div > div > div.w-full.xl\:w-72.lg\:mr-6.xl\:mr-12 > div > ul:nth-child(1) > li:nth-child(3)'))
-            )
-            print(driver.current_url)
-            # Extraction of status info 
-            status_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#main > div:nth-child(4) > div > div:nth-child(1) > div > div > div.w-full.xl\:w-72.lg\:mr-6.xl\:mr-12 > div > ul:nth-child(1) > li:nth-child(3)'))
-            )
-            print('fields loaded')
-            status_html = status_element.get_attribute('outerHTML')
-            status_info = extract_status(status_html)
-            
-            # Classification of orders
-            if status_info == 'Shipped' or status_info == 'Processing':
-                try:
-                    table = WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, '#simple-tracking-details > div > div > table > tbody'))
-                    )
-                    rows = table.find_elements(By.TAG_NAME, 'tr')
-                    table_data = []
-                    for row in rows:
-                        # Extract data from each cell in the row
-                        date = row.find_element(By.CSS_SELECTOR, 'td:nth-child(1) span').text.strip()
-                        carrier = row.find_element(By.CSS_SELECTOR, 'td:nth-child(2)').text.strip()
-                        tracking_number = row.find_element(By.CSS_SELECTOR, 'td:nth-child(3)').text.strip()
-                        tracking_link = row.find_element(By.CSS_SELECTOR, 'td:nth-child(4) a').get_attribute('href')
-
-                        # Append the extracted data to the list
-                        table_data.append({
-                            'Date': date,
-                            'Carrier': carrier,
-                            'Tracking Number': tracking_number,
-                            'Tracking Link': tracking_link
-                        })
-                except TimeoutError:
-                    # Handle case where table is not found
-                    table_data = 'None'
-            else:
-                table_data = 'None'
-
+                email_field.send_keys('alina.troian.proton@gmail.com')
+                order_number_field.send_keys(str(order_id))
+                print('keys sended')
+                # Wait for the submit button to be clickable
+                submit_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, '#track-orders-form > div > div > div > button'))
+                )
+                submit_button.click()
+                print('clicked button')
+                # Verify if the current page is the order status page
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, '#main > div:nth-child(4) > div > div:nth-child(1) > div > div > div.w-full.xl\:w-72.lg\:mr-6.xl\:mr-12 > div > ul:nth-child(1) > li:nth-child(3)'))
+                )
+                print(driver.current_url)
+                # Extraction of status info 
+                status_element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, '#main > div:nth-child(4) > div > div:nth-child(1) > div > div > div.w-full.xl\:w-72.lg\:mr-6.xl\:mr-12 > div > ul:nth-child(1) > li:nth-child(3)'))
+                )
+                print('fields loaded')
+                status_html = status_element.get_attribute('outerHTML')
+                status_info = extract_status(status_html)
                 
+                # Classification of orders
+                if status_info == 'Shipped' or status_info == 'Processing':
+                    try:
+                        table = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, '#simple-tracking-details > div > div > table > tbody'))
+                        )
+                        rows = table.find_elements(By.TAG_NAME, 'tr')
+                        table_data = []
+                        for row in rows:
+                            # Extract data from each cell in the row
+                            date = row.find_element(By.CSS_SELECTOR, 'td:nth-child(1) span').text.strip()
+                            carrier = row.find_element(By.CSS_SELECTOR, 'td:nth-child(2)').text.strip()
+                            tracking_number = row.find_element(By.CSS_SELECTOR, 'td:nth-child(3)').text.strip()
+                            tracking_link = row.find_element(By.CSS_SELECTOR, 'td:nth-child(4) a').get_attribute('href')
 
-            result_the_store[order_id] = [status_info, table_data]
+                            # Append the extracted data to the list
+                            table_data.append({
+                                'Date': date,
+                                'Carrier': carrier,
+                                'Tracking Number': tracking_number,
+                                'Tracking Link': tracking_link
+                            })
+                    except TimeoutError:
+                        # Handle case where table is not found
+                        table_data = 'None'
+                else:
+                    table_data = 'None'
 
-        except Exception as e:
-            print(f"An error occurred for order ID {order_id}: {e}")
-            result_the_store[order_id] = ['Error']
+                    
 
-        finally:
-            time.sleep(5)
-            counter_for_done += 1
-            print(f'Order done: {counter_for_done}/{number_of_orders}')
+                result_the_store[order_id] = [status_info, table_data]
 
-    driver.quit()
+            except Exception as e:
+                print(f"An error occurred for order ID {order_id}: {e}")
+                result_the_store[order_id] = ['Error']
+
+            finally:
+                time.sleep(5)
+                counter_for_done += 1
+                print(f'Order done: {counter_for_done}/{number_of_orders}')
+                
+    finally:
+        driver.quit()
+ 
     return result_the_store
 
 
